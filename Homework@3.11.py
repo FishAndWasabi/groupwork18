07417ed6cc3cc2a8b8fc030f215df17ce7d57d9e
JackWoodcym <chenym18@lzu.edu.cn>
2020-03-13 13:07:46 +0800

    Bug fix

--- Homework@3.11.py
+++ Homework@3.11.py
@@ -1,131 +1,73 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# get the commit count per sublevel pointwise or cumulative (c)
+# arguments is the tag as displayed by git tag and the number
+# of sublevels to be counted. If count is out of range for a
+# specific sublevel it will terminate the loop
+#
+# no proper header in this file
+# no legal/copyright ...OMG !
+#
+# things to cleanup:
+# restructure the code - use of functions
+# error handling ...where is the try..except ?
+# argument handling: you can do better right ?
+# documentation: once you understand it - fix the docs !
+# transform it into a class rather than just functions !
 
-"""
-get the commit count per sublevel pointwise or cumulative (c)
-arguments is the tag as displayed by git tag and the number
-of sublevels to be counted. If count is out of range for a
-specific sublevel it will terminate the loop
-"""
 
-__author__ = "Group No.18 in DSP of Lanzhou University"
-__copyright__ = "Copyright 2020, Study Project in Lanzhou University , China"
-__license__ = "GPL V3"
-__version__ = "0.1"
-__maintainer__ = "Yuming Chen"
-__email__ = "Chenym18@lzu.edu.cn"
-__status__ = "Experimental"
-
-import os, re, sys, subprocess, argparse
+import os, re, sys, subprocess
 from datetime import datetime as dt
-import pandas as pd
 
 
-class InvalidRevError(Exception):
-    """
-
-    """
-    pass
-
-
-class InvalidPathError(EnvironmentError):
-    """
-
-    """
-    pass
-
-
-def getArg():
-    """
-    Using argparse to handle the argument.
-    :return: args
-    """
-    parser = argparse.ArgumentParser(description="Count the commit")
-    parser.add_argument('-p', '--path', metavar='DIR', default='../linux/', help='path to Git Repository')
-    parser.add_argument('-r', '--rev', type=str, default='v4', help='First Reversion')
-    parser.add_argument('-g', '--rev-range', type=int, default=10, help='The range of the Reversion')
-    parser.add_argument('-c', '--cumulative', action='store_true', default=False, help='Enable cumulative arguments')
-    return parser.parse_args()
-
-
-def get_commit_cnt(outs):
-    """
-    Changes the
-    :param outs:
-    :return:
-    """
+def get_commit_cnt(git_cmd):
+    cnt = 0
+    raw_counts = git_cmd.communicate()[0]
     # if we request something that does not exist -> 0
-    cnt = re.findall('[0-9]*-[0-9]*-[0-9]*', str(outs))
+    cnt = re.findall('[0-9]*-[0-9]*-[0-9]*', str(raw_counts))
     return len(cnt)
 
 
-def get_tag_days(outs, base):
-    return ((int(outs) - base)) // 3600
+def get_tag_days(git_cmd, base):
+    seconds = git_cmd.communicate()[0]
+    return ((int(seconds) - base)) // 3600
 
 
-class Rep:
-    def __init__(self, args):
-        self.path = args.path
-        self.rev = args.rev
-        self.rev_range = args.rev_range
-        self.cumulative = args.cumulative
-        self.result = {}
-        # Verify the repository
-        try:
-            assert os.path.exists(os.path.join(self.path, '.git')), True
-        except AssertionError:
-            print('Invalid Git Repository')
-            raise InvalidPathError from None
+# get dates of all commits - unsorted
+rev = sys.argv[1]
+cumulative = 0
+if len(sys.argv) == 4:
+    if (sys.argv[3] == "c"):
+        cumulative = 1
+    else:
+        print("Dont know what you mean with %s" % sys.argv[3])
+        sys.exit(-1)
+rev_range = int(sys.argv[2])
 
-    def run_cmd(self, cmd):
-        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, shell=True,
-                             cwd=self.path)
-        try:
-            outs, errs = p.communicate(timeout=15)
-            if p.returncode:
-                print('Invalid Reversion')
-                raise subprocess.CalledProcessError(p.returncode, cmd) from None
-        except subprocess.TimeoutExpired:
-            p.kill()
-            raise RuntimeError("Timeout during get git commits") from None
-        return outs
+# setup and fill in the table
+print("#sublevel commits %s stable fixes" % rev)
+print("lv hour bugs")  # tag for R data.frame
+rev1 = rev
+# base time of v4.1 and v4.4 as ref base
+# fix this to extract the time of the base commit
+# from git !
+#
+# hofrat@Debian:~/git/linux-stable$ git log -1 --pretty=format:"%ct" v4.4
+# 1452466892
+v44 = 1452466892
 
-    def count(self):
-        # Init the 'result' dict
-        self.result['lv'] = []
-        self.result['hour'] = []
-        self.result['bugs'] = []
-        rev1 = self.rev + "." + str(0)
-        # Extract the time of the base commit from git
-        base = self.run_cmd("git log -1 --pretty=format:\"%ct\" " + rev1)
-        # Fill the 'result'
-        for sl in range(1, self.rev_range + 1):
-            rev2 = self.rev + "." + str(sl)
-            gitcnt = "git rev-list --pretty=format:\"%ai\" " + rev1 + "..." + rev2
-            gittag = "git log -1 --pretty=format:\"%ct\" " + rev2
-            git_rev_list = self.run_cmd(gitcnt)
-            commit_cnt = get_commit_cnt(git_rev_list)
-            if not self.cumulative:
-                rev1 = rev2
-            if commit_cnt:
-                git_tag_date = self.run_cmd(gittag)
-                days = get_tag_days(git_tag_date, int(base))
-                self.result['lv'].append(sl)
-                self.result['hour'].append(days)
-                self.result['bugs'].append(commit_cnt)
-            else:
-                break
-        # Create a data frame to store the count result which is better than printing directly.
-        df = pd.DataFrame(data=self.result)
-        # Reformat the Table
-        df.set_index('lv')  # Change index
-        title = "#sublevel commits %s stable fixes" % self.rev  # Add title
-        return title, df
-
-
-if __name__ == "__main__":
-    args = getArg()
-    r = Rep(args)
-    title, result = r.count()
-    print(title)
-    print(result)
+for sl in range(1, rev_range + 1):
+    rev2 = rev + "." + str(sl)
+    gitcnt = "git rev-list --pretty=format:\"%ai\" " + rev1 + "..." + rev2
+    gittag = "git log -1 --pretty=format:\"%ct\" " + rev2
+    git_rev_list = subprocess.Popen(gitcnt, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, shell=True)
+    commit_cnt = get_commit_cnt(git_rev_list)
+    if cumulative == 0:
+        rev1 = rev2
+    # if get back 0 then its an invalid revision number
+    if commit_cnt:
+        git_tag_date = subprocess.Popen(gittag, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, shell=True)
+        days = get_tag_days(git_tag_date, v44)
+        print("%d %d %d" % (sl, days, commit_cnt))
+    else:
+        break
